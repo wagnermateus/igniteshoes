9a600fec049fe240eb55e44bc582add87c12cead
wagnermateus <wagnermateus7@gmail.com>
2023-06-29 21:07:05 +0100

    Creating a cart tag

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -6,3 +6,7 @@ export function tagUserInfoCreate() {
     user_email: "wagnermateus7@gmail.com",
   });
 }
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.sendTag("cart_items_count", itemsCount);
+}
